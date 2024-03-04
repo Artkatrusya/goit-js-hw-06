27c323f39c9a9cfde1882fdf41b7cffa0eb1856a
Kate Bukata <rassokha.k@gmail.com>
2024-03-04 22:27:00 +0200

    v1.1

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const indexOfItem = this.#items.indexOf(itemToRemove);
+    if (indexOfItem != -1) {
+      this.#items = this.#items.slice(0, indexOfItem).concat(this.#items.slice(indexOfItem + 1, this.#items.length))
+    }  
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
